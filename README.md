ccf97a2968f254eac37c50fa5b5d9fcd880050f9
Skianyan <99053383+Skianyan@users.noreply.github.com>
2024-06-13 11:57:16 -0700

    new logic
    rewrote the script..

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # js-rps
+
 Rock Paper Scissors!
+
+Run this in live server to play a round of Rock Paper Scissors, first to 3 games wins!
--- main.js
+++ main.js
@@ -1,54 +1,78 @@
-let rndNum, playerSelect,cpuSelect;
-function computerPlay(rndNum){
-    rndNum = Math.random();
-    if (rndNum < .33){
-        cpuSelect = `rock`;
-    }
-    else if (rndNum >= .33 && rndNum <=.66){
-        cpuSelect = `paper`;
-    }
-    else{
-        cpuSelect = `scissors`;
-    }
-    console.log(`Cpu played ${cpuSelect.toUpperCase()}`);
+function getComputerChoice() {
+	let rndNum = Math.random();
+	if (rndNum <= 0.33) {
+		return "rock";
+	} else if (rndNum > 0.33 && rndNum <= 0.66) {
+		return "paper";
+	} else if (rndNum > 0.66) {
+		return "scissors";
+	}
 }
 
-function rpsRound(playerSelect,cpuSelect){
-    playerSelect = prompt(`Choose RPS`);
-    computerPlay(rndNum);
-    //p1Res=playerSelect.toLowerCase();
-    //console.log(p1Res);
-    console.log(playerSelect);
-    
-    if (playerSelect.localeCompare(cpuSelect, undefined, { sensitivity: 'base' }) == 0){
-        alert(`Tie ${playerSelect.toUpperCase()} ties with ${cpuSelect.toUpperCase()}`);
-    }
-    else if (playerSelect.toLowerCase() == `rock` && cpuSelect == `paper`){
-        alert(`You Lose, Paper beats Rock`);
-    }
-    else if (playerSelect.toLowerCase() == `rock` && cpuSelect == `scissors`){
-        alert(`You Win, Rock beats Scissors`);
-    }
-    else
-        alert(`bogos binted`);
+function getHumanChoice() {
+	let playerChoice = prompt("What will you play?");
+	if (
+		playerChoice.toLowerCase() == "rock" ||
+		playerChoice.toLowerCase() == "paper" ||
+		playerChoice.toLowerCase() == "scissors"
+	) {
+		return playerChoice.toLowerCase();
+	} else return console.error("insert a valid option");
 }
 
-rpsRound(playerSelect,cpuSelect);
+function playGame() {
+	let humanScore = 0,
+		cpuScore = 0;
 
-/* 
-Random Number Gen, if rndNum < .33 
-    display text "Rock"
-    if rndNum >= .33 && rndNum <=.66
-    display text "Paper"
-    else
-    display text "Scissors" 
+	function showScores() {
+		if (humanScore > cpuScore) console.log(humanScore + " to " + cpuScore);
+		else if (humanScore < cpuScore) console.log(humanScore + " to " + cpuScore);
+		else console.log(humanScore + " to " + cpuScore);
+	}
 
-Text comparison Test    
+	function humanWin(humanPlay, cpuPlay) {
+		humanScore = humanScore + 1;
+		console.log("You Win! " + humanPlay + " beats " + cpuPlay);
+	}
 
-    if (`paper`.localeCompare(`PaAper`, undefined, { sensitivity: 'base' }) == 0){
-    alert("photos printed");
+	function cpuWin(humanPlay, cpuPlay) {
+		cpuScore = cpuScore + 1;
+		console.log("You Lose, " + cpuPlay + " beats " + humanPlay);
+	}
+
+	function playRound() {
+		humanPlay = getHumanChoice();
+		cpuPlay = getComputerChoice();
+		if (humanPlay == "rock" && cpuPlay == "rock")
+			console.log("Tie, both played rock");
+		else if (humanPlay == "rock" && cpuPlay == "paper")
+			cpuWin(humanPlay, cpuPlay);
+		else if (humanPlay == "rock" && cpuPlay == "scissors")
+			humanWin(humanPlay, cpuPlay);
+		else if (humanPlay == "paper" && cpuPlay == "rock")
+			humanWin(humanPlay, cpuPlay);
+		else if (humanPlay == "paper" && cpuPlay == "paper")
+			console.log("Tie, both played paper");
+		else if (humanPlay == "paper" && cpuPlay == "scissors")
+			cpuWin(humanPlay, cpuPlay);
+		else if (humanPlay == "scissors" && cpuPlay == "rock")
+			cpuWin(humanPlay, cpuPlay);
+		else if (humanPlay == "scissors" && cpuPlay == "paper")
+			humanWin(humanPlay, cpuPlay);
+		else if (humanPlay == "scissors" && cpuPlay == "scissors")
+			console.log("Tie, both played scissors");
+		else console.error("an error occured during round");
+		showScores();
+	}
+
+	function endGame() {
+		console.log("Final Score: " + humanScore + " to " + cpuScore);
+	}
+	for (let i = 0; i < 5; i++) {
+		console.log("Round " + (i + 1));
+		playRound();
+	}
+	endGame();
 }
-else{
-    alert("bogos binted")
-}
-*/
+
+playGame();
